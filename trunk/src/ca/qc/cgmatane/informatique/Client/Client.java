76097646766241a42f6c12c85c0e2d83c7ddc8ba
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-10-14 17:23:46 +0000

    Creation d'un client et d'un serveur TCP

---
+++ trunk/src/ca/qc/cgmatane/informatique/Client/Client.java
@@ -0,0 +1,25 @@
+package ca.qc.cgmatane.informatique.Client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+class Client
+{
+    public static void main(String args[]) throws Exception
+    {
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("localhost", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        sentence = inFromUser.readLine();
+        outToServer.writeBytes(sentence + '\n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+    }
+}
