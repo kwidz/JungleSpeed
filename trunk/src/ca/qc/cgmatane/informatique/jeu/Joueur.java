eb6e514c793b86a271b7bffaff9d693a64d088ba
morgane.cabrol <morgane.cabrol@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-10-21 20:31:05 +0000

    modif constructeur joueur

--- trunk/src/ca/qc/cgmatane/informatique/jeu/Joueur.java
+++ trunk/src/ca/qc/cgmatane/informatique/jeu/Joueur.java
@@ -3,8 +3,8 @@ package ca.qc.cgmatane.informatique.jeu;
 public class Joueur {
     Paquet paquet = new Paquet();
 
-    public Joueur(Paquet p){
-        this.paquet = p;
+    public Joueur(){
+
     }
 
     public void modifierPaquet(Paquet nouveauP){
--- trunk/src/ca/qc/cgmatane/informatique/jeu/JoueurHumain.java
+++ trunk/src/ca/qc/cgmatane/informatique/jeu/JoueurHumain.java
@@ -2,7 +2,7 @@ package ca.qc.cgmatane.informatique.jeu;
 
 public class JoueurHumain extends Joueur{
     Paquet paquet = new Paquet();
-    public JoueurHumain(Paquet p ){
-        super(p);
+    public JoueurHumain( ){
+        super();
     }
 }
