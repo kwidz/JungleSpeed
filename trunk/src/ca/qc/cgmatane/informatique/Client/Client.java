66d859f2f1b59d4e1fe24fe63fc7e585105e89d4
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-10-25 19:18:39 +0000

    fonction d'envoi de score et de demande de score au serveur

--- trunk/src/ca/qc/cgmatane/informatique/Client/Client.java
+++ trunk/src/ca/qc/cgmatane/informatique/Client/Client.java
@@ -16,20 +16,54 @@ import java.net.Socket;
 
 public class Client
 {
-    public static void main(String args[]) throws Exception
-    {
+    public void demanderScore(){
         String sentence;
         String modifiedSentence;
+        DataOutputStream outToServer;
+        BufferedReader inFromServer;
         //BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("localhost", 6789);
+
+        outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
         sentence = new String("Test");
         creerDocumentdemanderScore();
         outToServer.writeBytes(sentence + '\n');
         modifiedSentence = inFromServer.readLine();
         System.out.println("FROM SERVER: " + modifiedSentence);
         clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void envoyerScore(String pseudoStr, int scoreStr){
+        String sentence;
+        String modifiedSentence;
+        DataOutputStream outToServer;
+        BufferedReader inFromServer;
+        //BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("localhost", 6789);
+
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            sentence = new String("Test");
+            creerDocumentEnvoyerScore(pseudoStr, scoreStr);
+            outToServer.writeBytes(sentence + '\n');
+            modifiedSentence = inFromServer.readLine();
+            System.out.println("FROM SERVER: \n" + modifiedSentence);
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static Document creerDocumentEnvoyerScore(String pseudoStr, int scoreStr){
