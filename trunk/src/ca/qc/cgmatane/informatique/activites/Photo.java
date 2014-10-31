f8fce3ade742b25b04e770bd52b3af782d643bbc
benjamin.heinen <benjamin.heinen@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-10-31 18:26:31 +0000

    maj enregistrement photo

--- trunk/src/ca/qc/cgmatane/informatique/activites/Photo.java
+++ trunk/src/ca/qc/cgmatane/informatique/activites/Photo.java
@@ -172,62 +172,91 @@ public class Photo extends Activity implements SurfaceHolder.Callback{
 	    }
 	}
 	private void SavePicture() {
-		    try {
-		        SimpleDateFormat timeStampFormat = new SimpleDateFormat(
-		                "yyyy-MM-dd-HH.mm.ss");
-		        String fileName = "photo_" + timeStampFormat.format(new Date())
-		                + ".jpg";
-		 
-		        // Metadata pour la photo
-		        ContentValues values = new ContentValues();
-		        values.put(Media.TITLE, fileName);
-		        values.put(Media.DISPLAY_NAME, fileName);
-		        values.put(Media.DESCRIPTION, "Image prise par FormationCamera");
-		        values.put(Media.DATE_TAKEN, new Date().getTime());
-		        values.put(Media.MIME_TYPE, "image/jpeg");
-		 
-		        // Support de stockage
-		        Uri taken = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI,
-		                values);
-            	System.out.println("taken : " + taken);
+	    try {
+	    /*	System.out.println("on rentre dans la sauvegarde du fichier");
+	        String fileName = "photoProfil" ;
+	 
+	        // Metadata pour la photo
+	        ContentValues values = new ContentValues();
+	        values.put(Media.TITLE, fileName);
+	        values.put(Media.DISPLAY_NAME, fileName);
+	        values.put(Media.DESCRIPTION, "photo de profil");
+	        values.put(Media.DATE_TAKEN, new Date().getTime());
+	        values.put(Media.MIME_TYPE, "image/jpeg");
+	 
+	        // Support de stockage
+	        System.out.println("a");
+	        Uri taken = getContentResolver().insert(Uri.parse("../res/drawable-hdpi"),
+	                values);
+	        System.out.println("b");
 
+	        System.out.println("taken : " + taken);
+	        if(taken == null)
+	        {
+	        	System.out.println("solution magique marche pas");
+	        }
+	        else
+	        {
+		        System.out.println("test : " + Media.EXTERNAL_CONTENT_URI);
+		        System.out.println("value : "+values);
+		        System.out.println("taken : " + taken);
 		        // Ouverture du flux pour la sauvegarde
 		        final FileOutputStream stream = (FileOutputStream) getContentResolver().openOutputStream(
 		                taken);
-            	System.out.println("stream : " + stream);
-
+		        System.out.println("stream : " + stream);
+	*/
 		        Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
 		        	 
 		            public void onPictureTaken(byte[] data, Camera camera) {
-		                if (data != null) {
-		                    // Enregistrement de votre image
-		                	System.out.println("data" + data);
-		                    try {
-		                        if (stream != null) {
-			                    	System.out.println("Le stream se cree bien");
-		                            stream.write(data);
-		                            stream.flush();
-		                            stream.close();
-		                        }
-		                    } catch (Exception e) {
-		                        // TODO: handle exception
-		                    	System.out.println("erreur1");
-		                    }
-		         
-		                    // On redémarre la prévisualisations
-		                    camera.startPreview();
-		                }
-		            }
-		        };
-		        camera.takePicture(null, pictureCallback, pictureCallback);
-		    } catch (Exception e) {
-		        // TODO: handle exception
-            	System.out.println("erreur2");
+		            	File sdCard = Environment.getExternalStorageDirectory();
+				        SimpleDateFormat timeStampFormat = new SimpleDateFormat(
+				                "yyyy-MM-dd-HH.mm.ss");
+				        String fileName = "photo_" + timeStampFormat.format(new Date())
+				                + ".jpg";
+		            	System.out.println("erreur1 : "+Environment.getExternalStorageDirectory());
+		    			File file = new File(sdCard, fileName);
+		     
+		    //Convertion 
+		    			InputStream is = new ByteArrayInputStream(data);
+		    			//BitmapFactory.Options opt = new BitmapFactory.Options();
+		    			//System.out.println("taill" +data.length);
+		    			Bitmap bMap = BitmapFactory.decodeStream(is);
+		    			FileOutputStream fileOutputStream;
+		            	System.out.println("erreur1:bonjour");
 
-		    }
-		 
-		}
+		    			try {
+		    				if (!file.exists()) {
+		    					System.out.println("erreur1 : test");
+		    					file.createNewFile();
+		    					System.out.println("erreur1 : sauf ");
+		    				}
+		    				System.out.println("erreur1 : " +file);
+		    				fileOutputStream = new FileOutputStream(file);
+		    				System.out.println("erreur1:bonjour2");
+		    				BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+		    				bMap.compress(CompressFormat.JPEG, 100, bos);
+		    				bos.flush();
+		    				bos.close();
+		    			} catch (FileNotFoundException e) {
+		    				// TODO Auto-generated catch block
+		    				System.out.println("erreur1");
+		    				e.printStackTrace();
+		    			} catch (IOException e) {
+		    				System.out.println("erreur2");
+
+		    				// TODO Auto-generated catch block
+		    				e.printStackTrace();
+		    			}
+		    		}
+		    	};
+
+		        camera.takePicture(null, null, pictureCallback);
+	    } catch (Exception e) {
+	        // TODO: handle exception
+	        System.out.println("MARRE");
+	    }
 	
+	}
 	private void finir()
 	{
 		this.finish();
