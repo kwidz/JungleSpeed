c5a4e9840402d1b2ad3aea5824a2eb6c1a7ffa58
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-01 18:25:00 +0000

    Imports innutiles

--- src/Serveur/ServeurNonXML.java
+++ src/Serveur/ServeurNonXML.java
@@ -1,14 +1,6 @@
 package Serveur;
 
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Created by kwidz on 01/11/14.
