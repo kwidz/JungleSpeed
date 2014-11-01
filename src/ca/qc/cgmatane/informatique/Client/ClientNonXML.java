b79339fd0bc2842ccbe96ebf6fb3a280ba80c15f
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-01 18:14:57 +0000

    avancement du client non xml

--- src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
+++ src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
@@ -1,7 +1,149 @@
 package ca.qc.cgmatane.informatique.Client;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by kwidz on 01/11/14.
  */
 public class ClientNonXML {
+    public void demanderScore(){
+
+        // Creation de la socket
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 6789);
+        } catch(UnknownHostException e) {
+            System.err.println("Erreur sur l'hôte : " + e);
+
+        } catch(IOException e) {
+            System.err.println("Creation de la socket impossible : " + e);
+
+        }
+
+        // Association d'un flux d'entree et de sortie
+        BufferedReader entree = null;
+        PrintWriter sortie = null;
+        try {
+            entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sortie = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        } catch(IOException e) {
+            System.err.println("Association des flux impossible : " + e);
+
+        }
+
+        Document document = creerDocumentdemanderScore();
+
+        // Envoi du document XML
+
+       String message = "Bonjour";
+        System.out.println("Envoi: " + message);
+        sortie.println(message);
+
+        message = new String();
+        try {
+            message= entree.readLine();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la lecture : " + e);
+            System.exit(-1);
+        }
+        System.out.println("Lu: " + message);
+        //lecture de la réponse au format xml
+
+        //fermetures
+
+        try {
+            entree.close();
+            sortie.close();
+            socket.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
+        }
+
+    }
+
+    public void envoyerScore(String pseudoStr, int scoreStr){
+        // Creation de la socket
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 6789);
+        } catch(UnknownHostException e) {
+            System.err.println("Erreur sur l'hôte : " + e);
+
+        } catch(IOException e) {
+            System.err.println("Creation de la socket impossible : " + e);
+
+        }
+
+        // Association d'un flux d'entree et de sortie
+        BufferedReader entree = null;
+        PrintWriter sortie = null;
+        try {
+            entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sortie = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        } catch(IOException e) {
+            System.err.println("Association des flux impossible : " + e);
+
+        }
+
+        String message = "Bonjour";
+        System.out.println("Envoi: " + message);
+        sortie.println(message);
+
+        //fermetures
+
+        try {
+            entree.close();
+            sortie.close();
+            socket.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
+        }
+    }
+
+    private Document creerDocumentEnvoyerScore(String pseudoStr, int scoreStr){
+
+        Element racine = new Element("requete");
+        Element type = new Element("type");
+        type.addContent("envoi");
+        Element score = new Element("score");
+        Element pseudo=new Element("pseudo");
+        Element pointage=new Element("pointage");
+        pointage.addContent(Integer.toString((scoreStr)));
+        pseudo.addContent(pseudoStr);
+        score.addContent(pseudo);
+        score.addContent(pointage);
+        racine.addContent(type);
+        racine.addContent(score);
+        Document document = new Document(racine);
+
+        return document;
+
+    }
+
+    private Document creerDocumentdemanderScore(){
+
+        Element racine = new Element("requete");
+        Element type = new Element("type");
+        type.addContent("demande");
+        racine.addContent(type);
+        Document document = new Document(racine);
+        try {
+            XMLOutputter afficheur = new XMLOutputter(Format.getPrettyFormat());
+            afficheur.output(document, System.out);
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'affichage : " + e);
+
+        }
+        return document;
+
+    }
 }
