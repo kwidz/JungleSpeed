b8e98fa83a292aa59c658285c392df51f958f6ab
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-01 19:20:00 +0000

    Serveur renvoie la chainne de carractere avec les scores

---
+++ trunk/src/test/TestClientNonXML.java
@@ -0,0 +1,16 @@
+package test;
+
+import ca.qc.cgmatane.informatique.Client.Client;
+import ca.qc.cgmatane.informatique.Client.ClientNonXML;
+
+/**
+ * Created by kwidz on 01/11/14.
+ */
+public class TestClientNonXML {
+    public static void main(String args[]) {
+
+        ClientNonXML client = new ClientNonXML();
+        //client.envoyerScore("geoffrey", 12);
+        client.demanderScore();
+    }
+}
