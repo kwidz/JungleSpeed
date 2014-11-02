d554676e655459b276a6c500338740acb44a18b2
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-02 22:35:30 +0000

    Envoi des Score dans un nouveau thread

--- trunk/src/ca/qc/cgmatane/informatique/Client/ThreadEnvoiScore.java
+++ trunk/src/ca/qc/cgmatane/informatique/Client/ThreadEnvoiScore.java
@@ -4,4 +4,12 @@ package ca.qc.cgmatane.informatique.Client;
  * Created by kwidz on 02/11/14.
  */
 public class ThreadEnvoiScore extends Thread {
+
+    public void run() {
+        try {
+            // pause
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+        }
+    }
 }
