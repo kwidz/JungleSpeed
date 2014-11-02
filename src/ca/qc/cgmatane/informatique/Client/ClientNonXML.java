fd82bf461b820fd4bf513d81b38c4fefd6590651
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-02 21:56:29 +0000

    Threadage du client android

--- src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
+++ src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
@@ -14,55 +14,18 @@ import java.util.StringTokenizer;
 public class ClientNonXML {
     public String demanderScore(){
 
-        // Creation de la socket
-        Socket socket = null;
-        try {
-            socket = new Socket("kwidz.fr", 6789);
-        } catch(UnknownHostException e) {
-            System.err.println("Erreur sur l'hôte : " + e);
+        ThreadReseau threadReseau = new ThreadReseau();
+        threadReseau.start();
+        while( threadReseau.isAlive() ) {
+            // faire un traitement...
 
-        } catch(IOException e) {
-            System.err.println("Creation de la socket impossible : " + e);
-
+            try {
+                // et faire une pause
+                threadReseau.sleep(800);
+            }
+            catch (InterruptedException ex) {}
         }
-
-        // Association d'un flux d'entree et de sortie
-        BufferedReader entree = null;
-        PrintWriter sortie = null;
-        try {
-            entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            sortie = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-        } catch(IOException e) {
-            System.err.println("Association des flux impossible : " + e);
-
-        }
-
-
-       String message = creerChaineDemandeScore();
-        System.out.println("Envoi: " + message);
-        sortie.println(message);
-
-        message = new String();
-        try {
-            message= entree.readLine();
-        } catch(IOException e) {
-            System.err.println("Erreur lors de la lecture : " + e);
-            System.exit(-1);
-        }
-        System.out.println("Lu: " + message);
-        //lecture de la réponse au format xml
-
-        //fermetures
-
-        try {
-            entree.close();
-            sortie.close();
-            socket.close();
-        } catch(IOException e) {
-            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
-        }
-
-        return message;
+        return threadReseau.getLeMessage();
 
     }
 
@@ -115,12 +78,7 @@ public class ClientNonXML {
 
     }
 
-    private String creerChaineDemandeScore(){
 
-        String demande="Demande:";
-        return demande;
-
-    }
     public Document retournerLesScores(){
 
         String lesScores=demanderScore();
---
+++ src/ca/qc/cgmatane/informatique/Client/ThreadReseau.java
@@ -0,0 +1,86 @@
+package ca.qc.cgmatane.informatique.Client;
+
+import org.jdom2.Document;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by kwidz on 02/11/14.
+ */
+public class ThreadReseau extends Thread{
+
+    String leMessage;
+
+    public String getLeMessage() {
+        return leMessage;
+    }
+
+    public void run(){
+        // Creation de la socket
+        Socket socket = null;
+        try {
+            socket = new Socket("kwidz.fr", 6789);
+        } catch(UnknownHostException e) {
+            System.err.println("Erreur sur l'hôte : " + e);
+
+        } catch(IOException e) {
+            System.err.println("Creation de la socket impossible : " + e);
+
+        }
+
+        // Association d'un flux d'entree et de sortie
+        BufferedReader entree = null;
+        PrintWriter sortie = null;
+        try {
+            entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sortie = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        } catch(IOException e) {
+            System.err.println("Association des flux impossible : " + e);
+
+        }
+
+
+        String message = creerChaineDemandeScore();
+        System.out.println("Envoi: " + message);
+        sortie.println(message);
+
+        message = new String();
+        try {
+            message= entree.readLine();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la lecture : " + e);
+            System.exit(-1);
+        }
+        System.out.println("Lu: " + message);
+        //lecture de la réponse au format xml
+
+        //fermetures
+
+        try {
+            entree.close();
+            sortie.close();
+            socket.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
+        }
+        leMessage=message;
+
+        try {
+            // pause
+            Thread.sleep(500);
+        }
+        catch (InterruptedException ex) {}
+
+
+
+    }
+
+    private String creerChaineDemandeScore(){
+
+        String demande="Demande:";
+        return demande;
+
+    }
+}
