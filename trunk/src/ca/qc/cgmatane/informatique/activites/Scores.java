27c0788c5886aa3776807db5ab9877a8d692c34a
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-02 21:21:32 +0000

    tableau des scores

--- trunk/src/ca/qc/cgmatane/informatique/activites/Scores.java
+++ trunk/src/ca/qc/cgmatane/informatique/activites/Scores.java
@@ -1,21 +1,26 @@
 package ca.qc.cgmatane.informatique.activites;
 
 import android.os.Bundle;
+import ca.qc.cgmatane.informatique.Client.ClientNonXML;
 import com.example.junglerapide.R;
 import android.app.Activity;
-import android.content.Intent;
-import android.opengl.GLSurfaceView;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scores extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scores);
-		
-	}
+        ClientNonXML client = new ClientNonXML();
+        Document document = client.retournerLesScores();
+        ArrayList<String> lesScores = new ArrayList<String>();
+        Element rootNode = document.getRootElement();
+        List<Element> listeDeScores = rootNode.getChildren("scores");
+
+    }
 
 }
