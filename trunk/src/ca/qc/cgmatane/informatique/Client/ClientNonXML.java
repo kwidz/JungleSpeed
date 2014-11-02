fece1f456c39b2a2909edb8cc366a3cf9ac5d522
geoffrey.glangine <geoffrey.glangine@04b5eff1-501a-4735-ae37-bee2a42fd26f>
2014-11-02 22:40:11 +0000

    Envoi des Score dans un nouveau thread

--- trunk/src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
+++ trunk/src/ca/qc/cgmatane/informatique/Client/ClientNonXML.java
@@ -29,53 +29,20 @@ public class ClientNonXML {
     }
 
     public void envoyerScore(String pseudoStr, int scoreStr){
-        // Creation de la socket
-        Socket socket = null;
-        try {
-            socket = new Socket("kwidz.fr", 6789);
-        } catch(UnknownHostException e) {
-            System.err.println("Erreur sur l'hôte : " + e);
+        ThreadEnvoiScore threadEnvoiScore = new ThreadEnvoiScore(pseudoStr, scoreStr);
+        threadEnvoiScore.start();
+        while( threadEnvoiScore.isAlive() ) {
+            // faire un traitement...
 
-        } catch(IOException e) {
-            System.err.println("Creation de la socket impossible : " + e);
-
-        }
-
-        // Association d'un flux d'entree et de sortie
-        BufferedReader entree = null;
-        PrintWriter sortie = null;
-        try {
-            entree = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            sortie = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-        } catch(IOException e) {
-            System.err.println("Association des flux impossible : " + e);
-
-        }
-
-        String message = creerChaineEnvoiScore(pseudoStr, scoreStr);
-        System.out.println("Envoi: " + message);
-        sortie.println(message);
-
-        //fermetures
-
-        try {
-            entree.close();
-            sortie.close();
-            socket.close();
-        } catch(IOException e) {
-            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
+            try {
+                // et faire une pause
+                threadEnvoiScore.sleep(800);
+            }
+            catch (InterruptedException ex) {}
         }
     }
 
-    private String creerChaineEnvoiScore(String pseudoStr, int scoreStr){
 
-        String envoiScore=new String();
-        envoiScore="Envoi:";
-        envoiScore+=pseudoStr+";"+Integer.toString(scoreStr);
-
-        return envoiScore;
-
-    }
 
 
     public Document retournerLesScores(){
